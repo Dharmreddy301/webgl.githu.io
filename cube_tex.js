2de656c820480ab66b19352a7215418a4871d396
Dharmreddy301 <127929337+Dharmreddy301@users.noreply.github.com>
2023-03-15 12:50:41 +0530

    Add files via upload

---
+++ cube_tex.js
@@ -0,0 +1,194 @@
+
+
+const canvas = document.querySelector('canvas');
+const gl = canvas.getContext('webgl');
+
+if (!gl) {
+    throw new Error('WebGL not supported');
+}
+
+  const vertexData = [
+
+    // Front
+    0.5, 0.5, 0.5, // top right 
+    0.5, -.5, 0.5, // bottom right
+    -.5, 0.5, 0.5, // top left
+    -.5, 0.5, 0.5, // top left
+    0.5, -.5, 0.5, // bottom right
+    -.5, -.5, 0.5, // bottom left
+
+    // Left
+    -.5, 0.5, 0.5,
+    -.5, -.5, 0.5,
+    -.5, 0.5, -.5,
+    -.5, 0.5, -.5,
+    -.5, -.5, 0.5,
+    -.5, -.5, -.5,
+
+    // Back
+    -.5, 0.5, -.5,
+    -.5, -.5, -.5,
+    0.5, 0.5, -.5,
+    0.5, 0.5, -.5,
+    -.5, -.5, -.5,
+    0.5, -.5, -.5,
+
+    // Right
+    0.5, 0.5, -.5,
+    0.5, -.5, -.5,
+    0.5, 0.5, 0.5,
+    0.5, 0.5, 0.5,
+    0.5, -.5, 0.5,
+    0.5, -.5, -.5,
+
+    // Top
+    0.5, 0.5, 0.5,
+    0.5, 0.5, -.5,
+    -.5, 0.5, 0.5,
+    -.5, 0.5, 0.5,
+    0.5, 0.5, -.5,
+    -.5, 0.5, -.5,
+
+    // Underside
+    0.5, -.5, 0.5,
+    0.5, -.5, -.5,
+    -.5, -.5, 0.5,
+    -.5, -.5, 0.5,
+    0.5, -.5, -.5,
+    -.5, -.5, -.5,
+];
+
+
+
+
+// Construct an Array by repeating `pattern` n times
+function repeat(n, pattern) {
+    return [...Array(n)].reduce(sum => sum.concat(pattern), []);
+}
+
+const uvData = repeat(6, [
+    1, 1, // top right
+    1, 0, // bottom right
+    0, 1, // top left
+
+    0, 1, // top left
+    1, 0, // bottom right
+    0, 0  // bottom left
+]);
+
+const positionBuffer = gl.createBuffer();
+gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertexData), gl.STATIC_DRAW);
+
+const uvBuffer = gl.createBuffer();
+gl.bindBuffer(gl.ARRAY_BUFFER, uvBuffer);
+gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(uvData), gl.STATIC_DRAW);
+
+function loadTexture(url) {
+    const texture = gl.createTexture();
+    const image = new Image();
+
+    image.onload = e => {
+        gl.bindTexture(gl.TEXTURE_2D, texture);
+        
+        gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image);
+
+        gl.generateMipmap(gl.TEXTURE_2D);
+    };
+
+    image.src = url;
+    return texture;
+}
+
+
+
+const brick = loadTexture(`default_brick.png`);
+gl.activeTexture(gl.TEXTURE0);
+gl.bindTexture(gl.TEXTURE_2D, brick);
+
+  
+let uniformLocations;
+(function shaderProgram() {
+    const vertexShader = gl.createShader(gl.VERTEX_SHADER);
+    gl.shaderSource(vertexShader, `
+    precision mediump float;
+
+    attribute vec3 position;
+    attribute vec2 uv;
+
+    varying vec2 vUV;
+
+    uniform mat4 matrix;
+
+    void main() {
+        vUV = uv;
+        gl_Position = matrix * vec4(position, 1);
+    }
+    `);
+    gl.compileShader(vertexShader);
+
+    const fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
+    gl.shaderSource(fragmentShader, `
+    precision mediump float;
+
+    varying vec2 vUV;
+    uniform sampler2D textureID;
+
+    void main() {
+        gl_FragColor = texture2D(textureID, vUV);
+    }
+    `);
+    gl.compileShader(fragmentShader);
+    console.log(gl.getShaderInfoLog(fragmentShader));
+
+    const program = gl.createProgram();
+    gl.attachShader(program, vertexShader);
+    gl.attachShader(program, fragmentShader);
+
+    gl.linkProgram(program);
+
+    const positionLocation = gl.getAttribLocation(program, `position`);
+    gl.enableVertexAttribArray(positionLocation);
+    gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+    gl.vertexAttribPointer(positionLocation, 3, gl.FLOAT, false, 0, 0);
+
+    const uvLocation = gl.getAttribLocation(program, `uv`);
+    gl.enableVertexAttribArray(uvLocation);
+    gl.bindBuffer(gl.ARRAY_BUFFER, uvBuffer);
+    gl.vertexAttribPointer(uvLocation, 2, gl.FLOAT, false, 0, 0);
+
+    gl.useProgram(program);
+    gl.enable(gl.DEPTH_TEST);
+
+    uniformLocations = {
+        matrix: gl.getUniformLocation(program, `matrix`),
+        textureID: gl.getUniformLocation(program, 'textureID'),
+    };
+
+    gl.uniform1i(uniformLocations.textureID, 0);
+})();
+
+const projectionMatrix = mat4.create();
+mat4.perspective(projectionMatrix, 75 * Math.PI / 180, canvas.width / canvas.height, 0.1, 100);
+const viewMatrix = mat4.create();
+mat4.translate(viewMatrix, viewMatrix, [0, 0.1, 2]);
+mat4.invert(viewMatrix, viewMatrix);
+const model= mat4.create();
+
+const mvMatrix = mat4.create();
+const mvpMatrix = mat4.create();
+
+function render() {
+    requestAnimationFrame(render);
+   
+    mat4.rotateX(model, model, Math.PI/60);
+    mat4.rotateY(model, model, Math.PI/160);
+    
+    mat4.multiply(mvMatrix, viewMatrix, model);
+    mat4.multiply(mvpMatrix, projectionMatrix, mvMatrix);
+    gl.uniformMatrix4fv(uniformLocations.matrix, false, mvpMatrix);
+    gl.drawArrays(gl.TRIANGLES, 0, vertexData.length / 3);
+    
+  }
+  
+render();
